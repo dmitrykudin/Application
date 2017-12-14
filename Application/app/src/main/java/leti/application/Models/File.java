f8daa75c48a1b85c1c356ac799c6bbc12ae6776b
Kristina <kristina_klimova96@mail.ru>
2017-12-14 21:59:55 +0300

    haha

---
+++ Application/app/src/main/java/leti/application/Models/File.java
@@ -0,0 +1,31 @@
+package leti.application.Models;
+
+/**
+ * Created by Дмитрий on 22.10.2017.
+ */
+
+public class File
+{
+    private String _path;
+    public String getPath() { return _path; }
+    public void setPath(String value)
+    {
+        if (!value.isEmpty())
+        {
+            _path = value;
+        }
+    }
+
+    private User _owner;
+    public User getOwner() { return _owner; }
+    public void setOwner(User value)
+    {
+        if (!value.equals(null))
+        {
+            _owner = value;
+        }
+    }
+
+    private String _fileextension;
+    public String getFileExtension() { return _fileextension; }
+}
