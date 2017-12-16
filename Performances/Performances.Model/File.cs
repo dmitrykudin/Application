b227bcb0ffbb28ddbb6adcce70e1dc871b204aaa
dmitrykudin <dim271096@gmail.com>
2017-12-16 17:19:04 +0300

    Revert "Revert "Bitmap to byte converter added""
    This reverts commit 4bbb99d16f11c62f9521fc6e93a53318a60bd0a9.

--- Performances/Performances.Model/File.cs
+++ Performances/Performances.Model/File.cs
@@ -8,6 +8,7 @@ namespace Performances.Model
 {
     public class File
     {
+        public int Id { get; set; }
         public string Path { get; set; }
         public int OwnerId { get; set; }
         public string FileExtension { get; set; }
